8e3eb00ae254e159d85c461fa7a2f4348ff9188f
Swathi <swathidanaboina7@gmail.com>
2025-10-31 21:45:28 +0530

    Initial commit

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ def search():
     results = cursor.fetchall()
     return render_template('results.html', scholarships=results)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
 
